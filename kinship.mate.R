80e15aa48b0ee485d6e7dc91222ad8c0a67e1949
yu052 <50414773+yu052@users.noreply.github.com>
2020-07-02 11:07:23 +0200

    Add files via upload

--- kinship.mate.R
+++ kinship.mate.R
@@ -1,20 +1,17 @@
 
 ### id of the target ancestor individual
-### wholeped is the complete pedigree records, given in string
-#wholeped<-"allped"
+
 
 kinship.mate<-function(id,individual,fa,mo){
-  individual<-as.character(individual)
-  fa<-as.character(fa)
-  mo<-as.character(mo)
+  individual<-as.character(individual);fa<-as.character(fa);mo<-as.character(mo)
+  n <- length(individual)
+  if (length(fa)!=n) stop("Mismatched length, individual and fa")
+  if (length(mo)!=n) stop("Mismatched length, individual and mo")
   allped<-data.frame(individual,fa,mo)
   colnames(allped)<-c("individual","sire","dam")
   allped$individual<-as.character(allped$individual);allped$sire<-as.character(allped$sire);allped$dam<-as.character(allped$dam)
   #allped<-get(wholeped)
   self<-allped[which(allped$individual==id),]
-  moNHSB<-as.character(allped[which(allped$individual==id),3])
-  faNHSB<-as.character(allped[which(allped$individual==id),2])
-  nrow_raw<-nrow(self)
   
   if((id %in% fa)&(id %in% mo) ){
     stop(paste(id," is both male and female, gender wrong"))
--- kinship.sibling.standard.R
+++ kinship.sibling.standard.R
@@ -1,12 +1,16 @@
 ### find full siblings and half-siblings
+### return the pedigree of sibling, including the full and half siblings, the father and mother as founders.
+### sib.type can be full, half, and all, means full-sibling only, half-sibling only and all sibling respectively.
 
 kinship.sibling<-function(id,individual,fa,mo){
   
-  individual<-as.character(individual)
-  fa<-as.character(fa)
-  mo<-as.character(mo)
+  individual<-as.character(individual);fa<-as.character(fa);mo<-as.character(mo)
+  n <- length(individual)
+  if (length(fa)!=n) stop("Mismatched length, individual and fa")
+  if (length(mo)!=n) stop("Mismatched length, individual and mo")
   allped<-data.frame(individual,fa,mo)
   colnames(allped)<-c("individual","sire","dam")
+  allped$individual<-as.character(allped$individual);allped$sire<-as.character(allped$sire);allped$dam<-as.character(allped$dam)
   
   self<-allped[which(allped$individual==id),]
   moID<-as.character(allped[which(allped$individual==id),3])
@@ -25,4 +29,54 @@ kinship.sibling<-function(id,individual,fa,mo){
   final_ped<-rbind(sibling,fa_mo)
   return(final_ped)
 }
-
+kinship.sibling<-function(id,individual,fa,mo,sib.type="all"){
+  
+  individual<-as.character(individual);fa<-as.character(fa);mo<-as.character(mo)
+  n <- length(individual)
+  if (length(fa)!=n) stop("Mismatched length, individual and fa")
+  if (length(mo)!=n) stop("Mismatched length, individual and mo")
+  allped<-data.frame(individual,fa,mo)
+  colnames(allped)<-c("individual","sire","dam")
+  allped$individual<-as.character(allped$individual);allped$sire<-as.character(allped$sire);allped$dam<-as.character(allped$dam)
+  
+  self<-allped[which(allped$individual==id),]
+  moID<-as.character(allped[which(allped$individual==id),3])
+  faID<-as.character(allped[which(allped$individual==id),2])
+  if (sib.type=="all"){
+    sibling_fa<-allped[which(allped$sire==faID),]
+    
+    sibling_mo<-allped[which(allped$dam==moID),]
+    
+    sibling<-unique(rbind(self,sibling_fa,sibling_mo))
+    
+    fa<-unique(as.character(sibling$sire))
+    mo<-unique(as.character(sibling$dam))
+    fa_mo<-data.frame("individual"=c(fa,mo),"sire"=rep(NA,length(c(fa,mo))),"dam"=rep(NA,length(c(fa,mo))))
+    #fa_mo<-apply(fa_mo, 2, function(y) as.character(y))
+    final_ped<-rbind(sibling,fa_mo)
+    return(final_ped)
+  } else if (sib.type=="full"){
+    sibling_full<-allped[which(allped$sire==faID & allped$dam==moID),]
+    sibling<-unique(rbind(self,sibling_full))
+    
+    fa<-unique(as.character(sibling$sire))
+    mo<-unique(as.character(sibling$dam))
+    fa_mo<-data.frame("individual"=c(fa,mo),"sire"=rep(NA,length(c(fa,mo))),"dam"=rep(NA,length(c(fa,mo))))
+    #fa_mo<-apply(fa_mo, 2, function(y) as.character(y))
+    final_ped<-rbind(sibling,fa_mo)
+    return(final_ped)
+  } else if (sib.type=="half"){
+    sibling_fa<-allped[which(allped$sire==faID & allped$dam!=moID),]
+    
+    sibling_mo<-allped[which(allped$dam==moID & allped$sire!=faID),]
+    
+    sibling<-unique(rbind(self,sibling_fa,sibling_mo))
+    
+    fa<-unique(as.character(sibling$sire))
+    mo<-unique(as.character(sibling$dam))
+    fa_mo<-data.frame("individual"=c(fa,mo),"sire"=rep(NA,length(c(fa,mo))),"dam"=rep(NA,length(c(fa,mo))))
+    #fa_mo<-apply(fa_mo, 2, function(y) as.character(y))
+    final_ped<-rbind(sibling,fa_mo)
+    return(final_ped)
+  }
+}
