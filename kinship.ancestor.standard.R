4e0df313282e3c339e53b1e222c046b94f78dae1
yu052 <50414773+yu052@users.noreply.github.com>
2020-07-02 10:39:46 +0200

    Add files via upload

--- kinship.ancestor.standard.R
+++ kinship.ancestor.standard.R
@@ -1,7 +1,7 @@
-### g indicate the number of generations to look up
-### id is the NHSB id of the target individual
+### g indicate the upper generations to find 
+### id is the id of the target individual
 
-kinship.ancestor<-function(g,id,individual,fa,mo){
+kinship.ancestor<-function(g=9999,id,individual,fa,mo){
   individual<-as.character(individual)
   fa<-as.character(fa)
   mo<-as.character(mo)
@@ -12,22 +12,31 @@ kinship.ancestor<-function(g,id,individual,fa,mo){
   fa<-allped[which(allped$individual==as.character(self$sire)),]
   mo<-allped[which(allped$individual==as.character(self$dam)),]
   ped<-rbind(self,fa,mo)
+  nrow_raw<-nrow(self)
   if (g == 1) {
+    ped[!(ped[,2] %in% ped[,1]),2:3]<-NA
+    ped[!(ped[,3] %in% ped[,1]),2:3]<-NA
     return(ped)
-    } else {
-  for (g in 2:g) {
-    number_individual<-nrow(ped)
-    for (i in 1:number_individual) {
-      fa<-allped[which(allped$individual==as.character(ped[i,2])),]# get info of every father in ped
-      mo<-allped[which(allped$individual==as.character(ped[i,3])),]# get info of every mother in ped
-      ped<-rbind(ped,fa,mo)
-      
+  } else {
+    for (g in 2:g) {
+      number_individual<-nrow(ped)
+      for (i in 1:number_individual) {
+        fa<-allped[which(allped$individual==as.character(ped[i,2])),]# get info of every father in ped
+        mo<-allped[which(allped$individual==as.character(ped[i,3])),]# get info of every mother in ped
+        ped<-rbind(ped,fa,mo)
+        
+      }
+      ped<-unique(ped)
+      nrow_new<-nrow(ped)
+      if (nrow_new==nrow_raw) {
+        print(paste(iter, "is the largest number of generation up of ",id ))
+        break # break when the size of the data frame don't increase, this means the end of the ancestor tracing
+      }
+      nrow_raw<-nrow_new
     }
-    ped<-unique(ped)
+    ped<-apply(ped, 2, function(y) as.character(y))
+    ped[!(ped[,2] %in% ped[,1]),2:3]<-NA
+    ped[!(ped[,3] %in% ped[,1]),2:3]<-NA
+    return(ped)
   }
-  ped<-apply(ped, 2, function(y) as.character(y))
-  ped[!(ped[,2] %in% ped[,1]),2:3]<-NA
-  ped[!(ped[,3] %in% ped[,1]),2:3]<-NA
-  return(ped)
- }
 }
